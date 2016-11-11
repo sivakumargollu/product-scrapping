01aadb1926a11fd67027188de8aaa41bd4211790
sivag <siva.gollu@imaginea.com>
2016-11-11 19:32:00 +0530

    Appconfig modification

---
+++ src/main/java/com/estrolo/scrapper/AppConfig.java
@@ -0,0 +1,24 @@
+package com.estrolo.scrapper;
+
+import org.apache.log4j.BasicConfigurator;
+
+import java.util.HashMap;
+
+/**
+ * Created by sivag on 11/11/16.
+ */
+public class AppConfig {
+    public static HashMap<String,String> props = new HashMap<String, String>();
+
+    static void init(){
+        props.put("site","http://www.flipkart.com");// Need to change scrapping logic.If you change it to other site
+        props.put("username","xxxx");//Modify
+        props.put("password","xxxx");//Modify
+        props.put("db","dbname");
+        props.put("dburl","url");
+        props.put("baseUri","http://localhost:7070");
+
+
+
+    }
+}
